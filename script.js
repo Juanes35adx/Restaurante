cee5d0ed77f2051c39a4c709c7b0fa8ae1b971ac
juane <juane.correa@upb.edu.co>
2026-03-02 21:14:08 -0500

    mejora diseño profesional restaurante

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-document.getElementById("form").addEventListener("submit", function(e){
-e.preventDefault();
-alert("Solicitud enviada. Pronto te contactaremos.");
+const elements = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+elements.forEach(el => {
+const position = el.getBoundingClientRect().top;
+const screen = window.innerHeight;
+
+if(position < screen - 100){
+el.style.opacity = 1;
+el.style.transform = "translateY(0)";
+}
+});
 });
